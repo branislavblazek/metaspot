bbebfc5d8dbc508a7ad27a3260cc64977c1d65e6
Branislav Blazek <blazek@pap.sk>
2021-09-14 23:18:57 +0200

    v0.3.1

--- screen/intro.py
+++ screen/intro.py
@@ -10,6 +10,7 @@ class Intro:
         self.active_index = 0
         self.exit_prompt_index = 0
         self.name = ''
+        self.score = 0
         self.choices = [
             {
                 'text': 'start ://metaspot',
@@ -72,7 +73,7 @@ class Intro:
         self.canvas.create_text(670,575, text="made by: branislav", font=c.SMALL_FONT, fill="green", anchor="nw")
 
         if self.active_choice == None:
-            self.canvas.create_text(50, 250, text="Welcome here {}!".format(self.name or '[no_username]'), font=c.REGULAR_FONT, fill='green', anchor='nw', tag=self.tag)
+            self.canvas.create_text(50, 250, text="Welcome here, {}! [x{}]".format(self.name or '[no_username]', self.score), font=c.REGULAR_FONT, fill='green', anchor='nw', tag=self.tag)
             Select(self.canvas, c.INTRO_SELECT_START[0], c.INTRO_SELECT_START[1], c.INTRO_SELECT_OFFSET, self.choices, self.handle_select_input, self.handle_select_enter, self.active_index, self.tag)
         elif self.active_choice == 'edit_name':
             Input(self.canvas, 450, 250, 700, self.choices[1], self.handle_name_enter, self.name, self.tag)
