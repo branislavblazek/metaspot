fa487aab55be1187646157ba0552c7ced5a87c57
Branislav Blazek <blazek@pap.sk>
2021-09-14 23:14:39 +0200

    v0.3

--- screen/intro.py
+++ screen/intro.py
@@ -1,37 +1,80 @@
 import consts as c
 from screen.select import Select
+from screen.prompt import Prompt
+from screen.input import Input
 
 class Intro:
     def __init__(self, canvas):
         self.canvas = canvas
+        self.tag = 'intro'
         self.active_index = 0
-        self.choices = ['edit your name', 'start ://metaspot']
+        self.exit_prompt_index = 0
+        self.name = ''
+        self.choices = [
+            {
+                'text': 'start ://metaspot',
+                'value': 'start',
+            },
+            {
+                'text': 'edit your name',
+                'value': 'edit_name',
+                'heading': 'Edit here your name:',
+            },
+            {
+                'text': 'exit',
+                'value': 'exit',
+                'heading': 'Are you sure?',
+                'data': [
+                    {
+                        'text': 'No!',
+                        'value': False,
+                    },
+                    {
+                        'text': 'Yes!',
+                        'value': True,
+                    }
+                ]
+            }
+        ]
+        self.active_choice = None
+        self.render()
+
+    def handle_select_enter(self, active_index):
+        value = self.choices[active_index]['value']
+        self.active_choice = value
+        self.render()
+
+    def handle_select_input(self, active_index):
+        self.active_index = active_index
+        self.render()
+
+    def handle_exit_enter(self, chosen_value):
+        if chosen_value == False:
+            self.active_choice = None
+            self.render()
+        elif chosen_value == True:
+            quit()
+
+    def handle_exit_input(self, active_index):
+        self.exit_prompt_index = active_index
+        self.render()
+
+    def handle_name_enter(self, new_name):
+        self.name = new_name
+        self.active_choice = None
+        self.render()
+
+    def render(self):
+        self.canvas.delete(self.tag)
 
         self.canvas.create_text(450,100, text="://metaspot",font=c.HEADING_FONT, fill='green')
-        self.canvas.create_text(10,575, text="version_0.2", font=c.SMALL_FONT, fill="green", anchor="nw")
+        self.canvas.create_text(10,575, text="version_0.3", font=c.SMALL_FONT, fill="green", anchor="nw")
         self.canvas.create_text(670,575, text="made by: branislav", font=c.SMALL_FONT, fill="green", anchor="nw")
 
-        self.render_select()
-
-        self.canvas.bind_all('<Up>', self.update_active_index)
-        self.canvas.bind_all('<Down>', self.update_active_index)
-
-    def render_select(self):
-        self.canvas.delete('intro_select')
-        Select(self.canvas, c.INTRO_SELECT_START[0], c.INTRO_SELECT_START[1], c.INTRO_SELECT_OFFSET, self.choices, self.active_index, 'intro_select')
-
-    def update_active_index(self, a):
-        add_number = -1 if a.keysym == 'Up' else 1 if a.keysym == 'Down' else 0
-
-        length = len(self.choices)
-
-        new_value = self.active_index + add_number
-
-        if new_value >= length:
-            self.active_index = length -1
-        elif new_value < 0:
-            self.active_index = 0
-        else:
-            self.active_index = new_value
-
-        self.render_select()
+        if self.active_choice == None:
+            self.canvas.create_text(50, 250, text="Welcome here {}!".format(self.name or '[no_username]'), font=c.REGULAR_FONT, fill='green', anchor='nw', tag=self.tag)
+            Select(self.canvas, c.INTRO_SELECT_START[0], c.INTRO_SELECT_START[1], c.INTRO_SELECT_OFFSET, self.choices, self.handle_select_input, self.handle_select_enter, self.active_index, self.tag)
+        elif self.active_choice == 'edit_name':
+            Input(self.canvas, 450, 250, 700, self.choices[1], self.handle_name_enter, self.name, self.tag)
+        elif self.active_choice == 'exit':
+            Prompt(self.canvas, 450, 250, 700, self.choices[2], self.handle_exit_input, self.handle_exit_enter, self.exit_prompt_index, self.tag)
